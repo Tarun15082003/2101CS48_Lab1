7100bf3acaab44be37678d24fc0b9833f33bdac3
tarun15082003 <nagireddy_2101cs48@iitp.ac.in>
2022-07-27 22:42:24 +0530

    Added the implementation code

--- calculator.c
+++ calculator.c
@@ -27,3 +27,34 @@ void division(long int int1, long int int2)
     printf("ERROR : division by 0\n");
 }
 }
+int main()
+{
+
+    printf("Enter first number :\n");
+    long int int1;
+    scanf("%ld", &int1);
+
+    long int int2;
+    printf("Enter second number :\n");
+    scanf("%ld", &int2);
+
+    printf("Enter code of the operation to be performed:\n 1. Add \t 2. Subtract \t 3. Multiply \t 4. Divide\n");
+    int code;
+    scanf("%d", &code);
+
+    switch (check)
+    {
+    case 1:
+        addition(int1, int2);
+        break;
+    case 2:
+        subtraction(int1, int2);
+        break;
+    case 3:
+        multiplication(int1, int2);
+        break;
+    case 4:
+        division(int1, int2);
+        break;
+    }
+}
